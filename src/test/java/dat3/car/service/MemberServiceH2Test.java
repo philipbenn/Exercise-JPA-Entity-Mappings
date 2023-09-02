f01f89291c972acedd65bf5add13c52866500745
philipbenn <phil658c@stud.kea.dk>
2023-09-02 15:08:40 +0200

    Added test

--- src/test/java/dat3/car/service/MemberServiceH2Test.java
+++ src/test/java/dat3/car/service/MemberServiceH2Test.java
@@ -1,151 +1,124 @@
 package dat3.car.service;
 
+import dat3.car.cars.dto.MemberRequest;
+import dat3.car.cars.dto.MemberResponse;
+import dat3.car.cars.entity.Member;
+import dat3.car.cars.repository.MemberRepository;
+import dat3.car.cars.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class MemberServiceH2Test {
 
-    /*@Autowired
+    @Autowired
     MemberRepository memberRepository;
+
     MemberService memberService;
 
-    Member m1, m2;  //Talk about references in Java for why we don't add the "isInitialize flag"
+    Member m1, m2;
 
     @BeforeEach
     void setUp() {
-        memberService = new MemberService(memberRepository); //Set up memberService with the mock (H2) database
-    }
-
-    private Member makeMember(String username, String password, String firstName, String lastName, String email, String street, String city, String zip) {
-        Member member = new Member(username, password, firstName, lastName, email, street, city, zip);
-        member.setCreated(LocalDateTime.now());
-        member.setEdited(LocalDateTime.now());
-        return member;
-    }
-
-    private Member makeMember2() {
+        memberService = new MemberService(memberRepository);
+        Member m1 = new Member("user1", "pw1", "fn1", "ln1", "email1", "street1", "city1", "zip1");
         Member m2 = new Member("user2", "pw2", "fn2", "ln2", "email2", "street2", "city2", "zip2");
-        m2.setCreated(LocalDateTime.now());
-        m2.setEdited(LocalDateTime.now());
-        return m2;
+        memberRepository.save(m1);
+        memberRepository.save(m2);
     }
-
 
     @Test
     void testGetMembersAllDetails() {
-        List<MemberResponse> memberResponses = memberService.getMembers(true);
-        assertEquals(2, memberResponses.size(), "Expects 2 members");
-        LocalDateTime time = memberResponses.get(0).getCreated();
-        assertNotNull(time, "Expects date to be set");
+        List<MemberResponse> allDetailsMembers = memberService.getMembers(true);
+
+        for (MemberResponse member : allDetailsMembers) {
+            assertNotNull(member.getUsername());
+            assertNotNull(member.getEmail());
+            assertNotNull(member.getFirstName());
+            assertNotNull(member.getLastName());
+            assertNotNull(member.getStreet());
+            assertNotNull(member.getCity());
+            assertNotNull(member.getZip());
+            assertNotNull(member.getCreated());
+            assertNotNull(member.getEdited());
+            assertNotNull(member.getRanking());
+            assertNotNull(member.getApproved());
+        }
     }
 
     @Test
     void testGetMembersNoDetails() {
-        List<MemberResponse> memberResponses = memberService.getMembers(false);
-        assertEquals(2, memberResponses.size(), "Expects 2 members");
-        LocalDateTime time = memberResponses.get(0).getCreated();
-        assertNull(time, "Expects date NOT to be set");
+        List<MemberResponse> noDetailMembers = memberService.getMembers(false);
+
+        for (MemberResponse member : noDetailMembers) {
+            assertNull(member.getCreated());
+            assertNull(member.getEdited());
+            assertNull(member.getRanking());
+            assertNull(member.getApproved());
+        }
     }
 
     @Test
     void testFindByIdFound() {
-        MemberResponse response = memberService.findById(m1.getUsername());
-        assertEquals(m1.getUsername(), response.getUsername());
+        MemberResponse response = memberService.findById("user1");
+        assertEquals("user1", response.getUsername());
     }
 
-    *//*@Test
+    @Test
     void testFindByIdNotFound() {
-        assertThrows(ResolutionException.class, () -> memberService.findById("I dont exist"));
-
-    }*//*
-
-    @Test
-        *//* Remember MemberRequest comes from the API layer, and MemberResponse is returned to the API layer
-         * Internally addMember savex a Member entity to the database*//*
-    void testAddMember_UserDoesNotExist() {
-        //Add @AllArgsConstructor to MemberRequest and @Builder to MemberRequest for this to work
-        //TODO
-    }
-
-    @Test
-    public void testAddMember_UserExists() {
-        when(memberRepository.existsById("user1")).thenReturn(true);
+        String nonExistentUsername = "nonExistentUser";
         assertThrows(ResponseStatusException.class, () -> {
-            Member existingMember = makeMember("user1", "pw1", "fn1", "ln1", "email1", "street1", "city1", "zip1");
-            //Make a MemberRequest with a username that already exists
-            MemberRequest mr = new MemberRequest(existingMember);
-            memberService.addMember(mr);
+            memberService.findById(nonExistentUsername);
         });
     }
 
     @Test
+    void testAddMember_UserDoesNotExist() {
+        Member member = new Member("sejephilip", "Password098", "philip@gmail.com", "Philip", "Philipsen",
+                "philipsgade 1", "Copenhagen", "zip");
+        MemberRequest memberRequest = new MemberRequest(member);
+        memberService.addMember(memberRequest);
+
+        assertEquals("Philip", member.getFirstName(), "Firstname expected");
+    }
+
+    @Test
     void testEditMemberWithExistingUsername() {
-        //TODO
+
     }
 
     @Test
     void testEditMemberNON_ExistingUsernameThrows() {
-        //This should test that a ResponseStatus exception is thrown with status= 404 (NOT_FOUND)
-        //TODO
+
     }
 
     @Test
     void testEditMemberChangePrimaryKeyThrows() {
-        //Create a MemberRequest from an existing member we can edit
-        MemberRequest request = new MemberRequest(m1);
-        //TODO
+
     }
 
     @Test
-    public void testSetRankingForUser_MemberFound() {
-        Member m1 = new Member();
-        m1.setUsername("user1");
-        when(memberRepository.findById("user1")).thenReturn(Optional.of(m1));
-        int testRanking = 5;
-
-        // Mock the behavior of memberRepository to save the testMember.
-        when(memberRepository.save(m1)).thenReturn(m1);
-        memberService.setRankingForUser("user1", testRanking);
-        assertEquals(testRanking, m1.getRanking());
-
-        // Verify that memberRepository's save method was called with the testMember.
-        verify(memberRepository).save(m1);
+    void testSetRankingForUser() {
+        memberService.setRankingForUser("user1", 10);
+        assertEquals(10, memberService.getMemberByUsername("user1").getRanking());
     }
 
     @Test
-    public void testSetRankingForUser_MemberNotFound() {
-        String testUsername = "testUser";
-        // Mock the behavior of memberRepository to return an empty Optional when findById is called with testUsername.
-        when(memberRepository.findById(testUsername)).thenReturn(Optional.empty());
-        // Assert that the method throws a ResponseStatusException with a BAD_REQUEST status.
-        assertThrows(ResponseStatusException.class, () -> memberService.setRankingForUser(testUsername, 5));
+    void testSetRankingForNoExistingUser() {
+        assertThrows(ResponseStatusException.class, () -> memberService.getMemberByUsername("user3").setRanking(10));
     }
 
     @Test
-    public void testDeleteMemberByUsername() {
-        String testUsername = "testUser";
-        Member testMember = new Member();
-        testMember.setUsername(testUsername);
-
-        // Mock the behavior of memberRepository to return the testMember when findById is called with testUsername.
-        when(memberRepository.findById(testUsername)).thenReturn(Optional.of(testMember));
-
-        // Call the method under test.
-        memberService.deleteMemberByUsername(testUsername);
-
-        // Verify that memberRepository's delete method was called with the testMember.
-        verify(memberRepository).delete(testMember);
+    void testDeleteMemberByUsername() {
+        memberService.deleteMemberByUsername("user1");
+        assertThrows(ResponseStatusException.class, () -> memberService.findById("user1"));
     }
-
-    @Test
-    public void testDeleteMemberByUsername_MemberNotFound() {
-        String testUsername = "testUser";
-
-        // Mock the behavior of memberRepository to return an empty Optional when findById is called with testUsername.
-        when(memberRepository.findById(testUsername)).thenReturn(Optional.empty());
-
-        // Assert that the method throws a ResponseStatusException with a BAD_REQUEST status.
-        assertThrows(ResponseStatusException.class, () -> memberService.deleteMemberByUsername(testUsername));
-    }*/
 }
 
